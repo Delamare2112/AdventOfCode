aceeb1b1b95aa2899f1402d7513bf2bd6acd8271
Delamare2112 <baladash@localhost.localdomain>
2015-12-01 22:53:49 -0700

    Day 1 part 2

--- 1/main.cpp
+++ 1/main.cpp
@@ -4,15 +4,28 @@
 
 int main()
 {
-	int number = 0;
+	int santaPosition = 0;
+	int basementCharacter = 0;
 	std::string line;
 	std::ifstream fileSteam("input.txt");
-	while(fileSteam.good())
+	if(fileSteam.good())
 	{
 		line = "";
 		fileSteam >> line;
-		for(char& c : line)
-			number += c == '(' ? 1 : -1;
+		for(int i = 0; i < line.length(); i++)
+		{
+			santaPosition += line[i] == '(' ? 1 : -1;
+			if(santaPosition < 0 && !basementCharacter)
+			{
+				basementCharacter = i + 1;
+			}
+		}
 	}
-	std::cout << number << std::endl;
+	std::cout << "Santa is on floor: " << santaPosition << std::endl;
+	std::cout << "Santa entered the basement at char: ";
+	if(basementCharacter)
+		std::cout << basementCharacter;
+	else
+		std::cout << "never";
+	std::cout << std::endl;
 }
