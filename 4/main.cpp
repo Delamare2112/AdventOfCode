de53d698fa000b8fff0f2682e904ccacf8f000d4
Delamare2112 <baladash@localhost.localdomain>
2015-12-05 00:22:33 -0700

    Day 4 Part 1

---
+++ 4/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "md5.h"
+
+int main(int argc, char const *argv[])
+{
+	if(argc == 1)
+	{
+		std::cout << "I need a secret key.\n";
+		return 1;
+	}
+
+	const std::string key = argv[1];
+	std::string result = "1234567890";
+	uint number = 0;
+	while(result.substr(0, 5) != "00000")
+	{
+		result = md5(key + std::to_string(number++));
+	}
+
+	std::cout << "Number: " << number - 1 << std::endl;
+
+	return 0;
+}
